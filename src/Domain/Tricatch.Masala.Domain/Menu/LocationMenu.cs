248a88028dbf128a386c04290c8aaaf3915cd6c0
Chris Tatlonghari <chris.tatlonghari.dev@gmail.com>
2024-03-19 17:32:52 -0400

    Add intial domain objects (#1)
    Co-authored-by: ChrisTatlonghari <Chris.Tatlonghari@fox.com>

---
+++ src/Domain/Tricatch.Masala.Domain/Menu/LocationMenu.cs
@@ -0,0 +1,14 @@
+﻿namespace Tricatch.Masala.Domain.Menu;
+
+public class LocationMenu
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Location { get; set; } = string.Empty;
+
+    public int ZipCode { get; set; }
+
+    public required List<MenuItem> Items { get; set; }
+}
