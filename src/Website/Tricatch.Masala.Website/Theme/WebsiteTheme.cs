2efc54ea349be5da27dfdbe1fb075eda8433db11
tri-catch <chris.tatlonghari.dev@gmail.com>
2024-03-20 23:23:06 -0400

    Add theme

---
+++ src/Website/Tricatch.Masala.Website/Theme/WebsiteTheme.cs
@@ -0,0 +1,19 @@
+﻿using MudBlazor;
+
+namespace Tricatch.Masala.Website.Theme;
+
+public class WebsiteTheme : MudTheme
+{
+
+    /// <summary>
+    /// Our site theme is an extension of Mudblazor's MudTheme. For a full list of Mudblazor's colors
+    /// see this page: https://mudblazor.com/features/colors#material-colors-list-of-material-colors
+    /// </summary>
+    public WebsiteTheme()
+    {
+        Palette = new PaletteLight()
+        {
+            AppbarBackground = Colors.Amber.Darken2
+        };
+    }
+}
