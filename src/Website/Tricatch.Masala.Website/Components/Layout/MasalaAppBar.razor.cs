44087e21e200731e6e33c85253027acdf759c5f8
tri-catch <chris.tatlonghari.dev@gmail.com>
2024-03-22 17:18:43 -0400

    Activate navigation tabs

--- src/Website/Tricatch.Masala.Website/Components/Layout/MasalaAppBar.razor.cs
+++ src/Website/Tricatch.Masala.Website/Components/Layout/MasalaAppBar.razor.cs
@@ -14,11 +14,29 @@ public partial class MasalaAppBar
 
     private Color _shoppingCartColor = Color.Dark;
 
+    private const string _menuText = "Menu";
+    private Color _menuTextColor = Color.Primary;
+
+    private const string _aboutText = "About";
+    private Color _aboutTextColor = Color.Dark;
+
+    private const string _contactUsText = "Contact Us";
+    private Color _contactUsTextColor = Color.Dark;
+
     private async Task DrawerToggle()
     {
         IsDrawerOpen = !IsDrawerOpen;
         _shoppingCartColor = IsDrawerOpen ? Color.Primary : Color.Dark;
 
         await OnDrawerToggle.InvokeAsync();
+    }
+
+    private void SetActiveTabColor(string activeTab)
+    {
+        _menuTextColor = activeTab == _menuText ? Color.Primary : Color.Dark;
+        _aboutTextColor = activeTab == _aboutText ? Color.Primary : Color.Dark;
+        _contactUsTextColor = activeTab == _contactUsText ? Color.Primary : Color.Dark;
+
+        StateHasChanged();
     }
 }
