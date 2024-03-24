e6d8bab8ae2f128c9832c48e40f0417e3231c0b1
tri-catch <chris.tatlonghari.dev@gmail.com>
2024-03-24 14:30:38 -0400

    Add menu category panel

---
+++ src/Website/Tricatch.Masala.Website/Components/MenuCategoryPanel.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Tricatch.Masala.Website.Components;
+
+public partial class MenuCategoryPanel
+{
+    [Parameter]
+    public EventCallback<int> CategoryChanged { get; set; }
+
+    private int _activeTabIndex { get; set; }
+
+    protected override void OnInitialized()
+    {
+        _activeTabIndex = 0;
+    }
+
+    private async Task OnTabChanged(int tabIndex)
+    {
+        _activeTabIndex = tabIndex;
+        await CategoryChanged.InvokeAsync(tabIndex);
+    }
+}
--- src/Website/Tricatch.Masala.Website/Components/Pages/Menu.razor.cs
+++ src/Website/Tricatch.Masala.Website/Components/Pages/Menu.razor.cs
@@ -4,15 +4,23 @@ using Tricatch.Masala.Domain.Menu;
 
 namespace Tricatch.Masala.Website.Components.Pages;
 
-public partial class Home
+public partial class Menu
 {
     [Inject]
-    IMenuItemRepository MenuItemRepository { get; set; }
+    IMenuItemRepository? MenuItemRepository { get; set; }
 
-    private IEnumerable<MenuItem> _menuItems { get; set; }
+    private IEnumerable<MenuItem>? _menuItems { get; set; }
+    private int _selectedCategoryId { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
-        _menuItems = await MenuItemRepository.GetMenuItemsAsync();
+        _menuItems = await MenuItemRepository!.GetMenuItemsAsync();
+        _selectedCategoryId = 0;
     }
+
+    private void OnCategorySwitched(int categoryId) 
+    {
+        _selectedCategoryId = categoryId;
+    }
+
 }
